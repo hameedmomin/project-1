3517e89d5bf880fc387d1d66fec8771c004d63e8
USER <hameed.momin@gmail.com>
2022-04-25 09:50:27 +0530

    dev

--- stroage4/01-version.tf
+++ stroage4/01-version.tf
@@ -17,7 +17,7 @@ terraform {
   backend "azurerm" {
     resource_group_name   = "azurerm_resource_group.storage.name"
     storage_account_name  = "azurerm_storage_account.new-sa.name"
-    container_name        = "azurerm_storage_container.backend.name"
+    container_name        = "terraformstatefile"
     key                   = "project-1-eastus2-terraform.tfstate"
   }
 }
