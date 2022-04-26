03a3f3f5d55a69e84f2d5e77758aea581d0730f8
USER <hameed.momin@gmail.com>
2022-04-26 12:38:58 +0530

    dev

--- lb/02.main.tf
+++ lb/02.main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "project2" {
   location                                     = "eastus2"
   name                                         = "${var.PREFIX}-${var.ENV}"
 }
-/*resource "random_string" "locals" {
+resource "random_string" "locals" {
   length                                       = 16
   number                                       = false
   lower                                        = false
@@ -10,5 +10,5 @@ resource "azurerm_resource_group" "project2" {
   special                                      = false
   override_special                             = "-/@"
 
-}*/
+}
 
